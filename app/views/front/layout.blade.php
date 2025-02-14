10ff2df24ad5cca35db9b619a7d33847d4772396
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-02-14 10:39:41 +0100

    appliquer js dans un fichier script.js

--- app/views/front/layout.blade.php
+++ app/views/front/layout.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="../../../assets/js/script.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <style>
@@ -53,6 +52,8 @@
 
   </main>
 
+
+  <script src="../../../assets/js/script.js"></script>
   @include('front.partials.footer')
   @yield('scripts')
 </body>
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -8,6 +8,40 @@
         document.body.classList.remove('no-scroll'); 
     }
 
+    document.addEventListener("DOMContentLoaded", function () {
+        const typeDropdown = document.getElementById('type');
+        const eventTypeDropdown = document.getElementById('event_type');
+        const prixField = document.getElementById('prixField');
+        const localisationField = document.getElementById('localisationField');
+        const lienField = document.getElementById('lienField');
+    
+        if (typeDropdown && eventTypeDropdown) {
+            typeDropdown.addEventListener('change', togglePrixField);
+            eventTypeDropdown.addEventListener('change', toggleEventTypeFields);
+        }
+    
+        function togglePrixField() {
+            if (typeDropdown.value === 'payant') {
+                prixField.classList.remove('hidden'); 
+            } else {
+                prixField.classList.add('hidden');
+            }
+        }
+    
+        function toggleEventTypeFields() {
+            if (eventTypeDropdown.value === 'live') {
+                lienField.classList.remove('hidden'); 
+                localisationField.classList.add('hidden');
+            } else if (eventTypeDropdown.value === 'presentiel') {
+                localisationField.classList.remove('hidden');
+                lienField.classList.add('hidden'); 
+            } else {
+                localisationField.classList.add('hidden');
+                lienField.classList.add('hidden');
+            }
+        }
+    });
+    
    
 
     function openForms() {
@@ -19,5 +53,37 @@
         document.getElementById('formPopups').classList.add('hidden');
         document.body.classList.remove('no-scroll');
     }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const regionSelect = document.getElementById('region-select');
+        const villeSelect = document.getElementById('ville-select');
+    
+        if (regionSelect && villeSelect) {
+            regionSelect.addEventListener('change', function() {
+                const regionId = this.value;
+                villeSelect.innerHTML = '<option value="">Sélectionner une ville</option>';
+    
+                if (regionId) {
+                    const xhr = new XMLHttpRequest();
+                    xhr.open('GET', 'get_villes?id=' + regionId, true);
+                    xhr.onreadystatechange = function() {
+                        if (xhr.readyState === 4 && xhr.status === 200) {
+                            const villes = JSON.parse(xhr.responseText);
+                            villes.forEach(ville => {
+                                const option = document.createElement('option');
+                                option.value = ville.id;
+                                option.textContent = ville.name;
+                                villeSelect.appendChild(option);
+                            });
+                        }
+                    };
+                    xhr.send();
+                }
+            });
+        } else {
+            console.error("region-select or ville-select not found in the DOM.");
+        }
+    });
+    
 
     
