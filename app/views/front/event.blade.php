b0f9ece3b0aef0557e1ad9e2d8f152b860932aae
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-02-14 09:29:26 +0100

    fixer home page

--- app/views/front/event.blade.php
+++ app/views/front/event.blade.php
@@ -321,6 +321,36 @@
         }
         });
 
+        const typeDropdown = document.getElementById('type');
+    const eventTypeDropdown = document.getElementById('event_type');
+    const prixField = document.getElementById('prixField');
+    const localisationField = document.getElementById('localisationField');
+    const lienField = document.getElementById('lienField');
+
+    typeDropdown.addEventListener('change', togglePrixField);
+    eventTypeDropdown.addEventListener('change', toggleEventTypeFields);
+
+    function togglePrixField() {
+        if (typeDropdown.value === 'payant') {
+            prixField.classList.remove('hidden'); 
+        } else {
+            prixField.classList.add('hidden');
+        }
+    }
+
+    function toggleEventTypeFields() {
+        if (eventTypeDropdown.value === 'live') {
+            lienField.classList.remove('hidden'); 
+            localisationField.classList.add('hidden');
+        } else if (eventTypeDropdown.value === 'presentiel') {
+            localisationField.classList.remove('hidden');
+            lienField.classList.add('hidden'); 
+        } else {
+            localisationField.classList.add('hidden');
+            lienField.classList.add('hidden');
+        }
+    }
+
     </script>
 </body>
 </html>
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -8,35 +8,7 @@ function openForm() {
         document.body.classList.remove('no-scroll'); 
     }
 
-    const typeDropdown = document.getElementById('type');
-    const eventTypeDropdown = document.getElementById('event_type');
-    const prixField = document.getElementById('prixField');
-    const localisationField = document.getElementById('localisationField');
-    const lienField = document.getElementById('lienField');
-
-    typeDropdown.addEventListener('change', togglePrixField);
-    eventTypeDropdown.addEventListener('change', toggleEventTypeFields);
-
-    function togglePrixField() {
-        if (typeDropdown.value === 'payant') {
-            prixField.classList.remove('hidden'); 
-        } else {
-            prixField.classList.add('hidden');
-        }
-    }
-
-    function toggleEventTypeFields() {
-        if (eventTypeDropdown.value === 'live') {
-            lienField.classList.remove('hidden'); 
-            localisationField.classList.add('hidden');
-        } else if (eventTypeDropdown.value === 'presentiel') {
-            localisationField.classList.remove('hidden');
-            lienField.classList.add('hidden'); 
-        } else {
-            localisationField.classList.add('hidden');
-            lienField.classList.add('hidden');
-        }
-    }
+   
 
     function openForms() {
         document.getElementById('formPopups').classList.remove('hidden');
